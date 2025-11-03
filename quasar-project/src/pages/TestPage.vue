a32f4f1a64cb6c4e5b01d1077b9842122f262bbf
Gabrijel69 <119818187+Gabrijel69@users.noreply.github.com>
2025-11-03 11:37:58 +0100

    Refactor TestPage.vue with input fields and logging

--- quasar-project/src/pages/TestPage.vue
+++ quasar-project/src/pages/TestPage.vue
@@ -1,12 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Test Page</h1>
+  <q-page padding>
 
-    <div>{{poruka}}</div>
-
+    <q-input color="purple-12" v-model="text1" label="item name" @keyup="log()">
+    </q-input>
+    <q-input color="purple-12" v-model="text1" label="item name" @keyup="log()">
+    </q-input>
   </q-page>
 </template>
 
 <script setup>
-const poruka = "Test page"
+import {ref} from "vue"
+  const text1 = ref('')
+  text1.value = 'Hello'
+
+  function log() {
+    console.log(text1.value)
+  }
 </script>
